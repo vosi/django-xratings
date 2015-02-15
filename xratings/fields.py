a8c6cccdf2906836a7ce6e8fcd4516870e325ad7
Volodymyr Tartynskyi <vosi@users.noreply.github.com>
2015-02-16 00:14:31 +0200

    Update fields.py

--- xratings/fields.py
+++ xratings/fields.py
@@ -1,86 +1,96 @@
-from django.db.models import IntegerField, FloatField
+# coding=utf-8
+from __future__ import unicode_literals
+
+from hashlib import md5
 from django.conf import settings
+
+from django.utils.six import python_2_unicode_compatible
+from django.utils.timezone import now
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import IntegerField, FloatField
 from django_extensions.db.fields.json import JSONField
 
-import forms
-import itertools
-from datetime import datetime
-
-from models import Vote
-from default_settings import RATINGS_VOTES_PER_IP, RATINGS_DEFAULT_FORMULA, \
+from ratings.default_settings import RATINGS_VOTES_PER_IP
+from ratings.models import Vote
+from ratings.exceptions import (InvalidRating, CannotDeleteVote, AuthRequired,
+                                IPLimitReached, CannotChangeVote)
+from ratings.default_settings import RATINGS_DEFAULT_FORMULA, \
     RATINGS_VOTES_PER_IP_TIMEDELTA
-from exceptions import *
 
 if 'django.contrib.contenttypes' not in settings.INSTALLED_APPS:
-    raise ImportError("xratings requires django.contrib.contenttypes in "
-                      "your INSTALLED_APPS")
+    raise ImportError('xratings requires django.contrib.contenttypes in your '
+                      'INSTALLED_APPS')
 
-from django.contrib.contenttypes.models import ContentType
 
 __all__ = ('Rating', 'RatingField', 'AnonymousRatingField')
-
-try:
-    from hashlib import md5
-except ImportError:
-    from md5 import new as md5
-
-try:
-    from django.utils.timezone import now
-except ImportError:
-    now = datetime.now
 
 
 def md5_hexdigest(value):
     return md5(value).hexdigest()
 
 
+class Rating(object):
+    def __init__(self, score, votes):
+        self.scores = None
+        self.score_month = None
+        self.score_week = None
+        self.score_day = None
+        self.score = score
+        self.votes = votes
+
+
+@python_2_unicode_compatible
 class RatingManager(object):
     def __init__(self, instance, field):
-        self.field = field
-        self.instance = instance
         self.content_type = None
+        self.instance = instance
+        self.field = field
 
-        self.score_field_name = "%s_score" % (self.field.name,)
-        self.score_day_field_name = "%s_day" % (self.field.name,)
-        self.score_week_field_name = "%s_week" % (self.field.name,)
-        self.score_month_field_name = "%s_month" % (self.field.name,)
-        self.scores_field_name = "%s_scores" % (self.field.name,)
+        self.score_field_name = '%s_score' % (self.field.name,)
+        self.score_day_field_name = '%s_day' % (self.field.name,)
+        self.score_week_field_name = '%s_week' % (self.field.name,)
+        self.score_month_field_name = '%s_month' % (self.field.name,)
+        self.scores_field_name = '%s_scores' % (self.field.name,)
 
     def __str__(self):
-        return "%d" % self.get_rating()
-
-    def __unicode__(self):
-        return unicode(self.get_rating())
+        return '%d' % self.get_rating()
 
     def get_ratings(self):
+        # Returns a Vote QuerySet for this rating field.
         return Vote.objects.filter(content_type=self.get_content_type(),
                                    object_id=self.instance.pk,
                                    key=self.field.key)
 
     def get_rating(self):
+        # Returns the calculated rating.
         return self.field.rating_calculator(self.scores, self.field.range)
 
     def get_real_rating(self):
+        # Returns the unmodified rating.
         if len(self.scores) == 0:
             return 0
         return sum([a * b for a, b in zip(self.field.range, self.scores)])
 
     def get_sum_votes(self):
+        # Returns number of votes
         return sum(self.scores)
 
     def get_avg_rating(self):
-        if sum(self.scores) == 0:
+        # Returns simple avg rating
+        if self.get_sum_votes() == 0:
             return 0
-        return float(self.get_real_rating()) / sum(self.scores)
+        return float(self.get_real_rating()) / self.get_sum_votes()
 
-    def get_rating_for_user(self, user, ip_address=None, cookies={}):
-        kwargs = dict(
-            content_type=self.get_content_type(),
-            object_id=self.instance.pk,
-            key=self.field.key,
-        )
+    def get_rating_for_user(self, user, ip_address=None, cookies=None):
+        # Returns the rating for a user or anonymous IP.
+        if not cookies:
+            cookies = {}
+        kwargs = {
+            'content_type': self.get_content_type(),
+            'object_id': self.instance.pk,
+            'key': self.field.key
+        }
 
-        #TODO: IP address is not mandatory
         if not (user and user.is_authenticated()):
             if not ip_address:
                 raise ValueError('``user`` or ``ip_address`` must be present.')
@@ -94,13 +104,12 @@ class RatingManager(object):
             # TODO: move 'vote-%d.%d.%s' to settings or something
             cookie_name = 'vote-%d.%d.%s' % (
                 kwargs['content_type'].pk, kwargs['object_id'],
-                kwargs['key'][:6],) # -> md5_hexdigest?
+                kwargs['key'][:6],)  # -> md5_hexdigest?
             cookie = cookies.get(cookie_name)
             if cookie:
                 kwargs['cookie'] = cookie
             else:
                 kwargs['cookie__isnull'] = True
-
         try:
             rating = Vote.objects.get(**kwargs)
             return rating.score
@@ -110,43 +119,42 @@ class RatingManager(object):
             pass
         return
 
-    def add(self, score, user, ip_address, cookies=None):
+    def add(self, score, user, ip_address, cookies=None, commit=True):
         if not cookies:
             cookies = {}
 
         try:
             score = int(score)
         except (ValueError, TypeError):
-            raise InvalidRating("%s is not a valid choice for %s, "
-                                "type missmatch" % (score, self.field.name))
+            raise InvalidRating('%s is not a valid choice for %s' %
+                                (score, self.field.name))
 
         delete = (score == 0)
         if delete and not self.field.allow_delete:
-            raise CannotDeleteVote("you are not allowed to delete "
-                                   "votes for %s" % (self.field.name,))
+            raise CannotDeleteVote('you are not allowed to delete votes for %s' %
+                                   (self.field.name,))
 
         if not self.field.check_range(score):
-            raise InvalidRating("%s is not a valid choice for %s, score "
-                                "not in range" % (score, self.field.name))
+            raise InvalidRating('%s is not a valid choice for %s' %
+                                (score, self.field.name))
 
         is_anonymous = (user is None or not user.is_authenticated())
         if is_anonymous and not self.field.allow_anonymous:
-            raise AuthRequired("user must be a user, not '%r'" % (user,))
+            raise AuthRequired('user must be a user, not `%r`' % (user,))
 
         if is_anonymous:
             user = None
 
-        defaults = dict(
-            score=score,
-            ip_address=ip_address,
-        )
-
-        kwargs = dict(
-            content_type=self.get_content_type(),
-            object_id=self.instance.pk,
-            key=self.field.key,
-            user=user,
-        )
+        defaults = {
+            'score': score,
+            'ip_address': ip_address
+        }
+        kwargs = {
+            'content_type': self.get_content_type(),
+            'object_id': self.instance.pk,
+            'key': self.field.key,
+            'user': user
+        }
         if not user:
             kwargs['ip_address'] = ip_address
 
@@ -165,8 +173,9 @@ class RatingManager(object):
             rating, created = Vote.objects.get(**kwargs), False
         except Vote.DoesNotExist:
             if delete:
-                raise CannotDeleteVote("attempt to find and delete your vote "
-                                       "for %s is failed" % (self.field.name,))
+                raise CannotDeleteVote(
+                    'attempt to find and delete your vote for %s is failed' %
+                    (self.field.name,))
             if RATINGS_VOTES_PER_IP > 0:
                 num_votes = Vote.objects.filter(
                     content_type=kwargs['content_type'],
@@ -183,6 +192,8 @@ class RatingManager(object):
                 kwargs.pop('cookie__isnull', '')
             rating, created = Vote.objects.create(**kwargs), True
 
+        # self.score = self.get_rating()
+
         if not created:
             if self.field.can_change_vote:
                 self.scores[self.field.range.index(rating.score)] -= 1
@@ -193,9 +204,7 @@ class RatingManager(object):
             else:
                 raise CannotChangeVote()
         else:
-            if not (isinstance(self.scores, list) and
-                            len(self.scores) == len(
-                                    [0] * len(self.field.range))):
+            if not (isinstance(self.scores, list) and len(self.scores) == len([0] * len(self.field.range))):
                 self.scores = [0] * len(self.field.range)
             self.scores[self.field.range.index(score)] += 1
 
@@ -210,8 +219,7 @@ class RatingManager(object):
             'score_sum': self.get_real_rating(),
             'scores': self.scores,
             'deleted': delete,
-            'created': created
-        }
+            'created': created}
         if use_cookies:
             adds['cookie_name'] = cookie_name
             adds['cookie'] = cookie
@@ -221,10 +229,12 @@ class RatingManager(object):
         self.scores[self.field.range.index(score)] -= 1
         self.score = self.get_rating()
 
-    def _get_score(self, default=None):
+    @property
+    def score(self, default=None):
         return getattr(self.instance, self.score_field_name, default)
 
-    def _set_score(self, value):
+    @score.setter
+    def score(self, value):
         old = getattr(self.instance, self.score_field_name, 0)
 
         old_day = getattr(self.instance, self.score_day_field_name, 0)
@@ -241,19 +251,17 @@ class RatingManager(object):
 
         setattr(self.instance, self.score_field_name, value)
 
-    score = property(_get_score, _set_score)
-
-    def _get_scores(self, default=None):
+    @property
+    def scores(self, default=None):
         return getattr(self.instance, self.scores_field_name, default)
 
-    def _set_scores(self, value):
+    @scores.setter
+    def scores(self, value):
         setattr(self.instance, self.scores_field_name, value)
-
-    scores = property(_get_scores, _set_scores)
 
     def get_content_type(self):
         if self.content_type is None:
-            self.content_type = ContentType.objects \
+            self.content_type = ContentType.objects\
                 .get_for_model(self.instance)
         return self.content_type
 
@@ -261,20 +269,26 @@ class RatingManager(object):
 class RatingCreator(object):
     def __init__(self, field):
         self.field = field
+        self.score_field_name = '%s_score' % (self.field.name,)
+        self.score_day_field_name = '%s_day' % (self.field.name,)
+        self.score_week_field_name = '%s_week' % (self.field.name,)
+        self.score_month_field_name = '%s_month' % (self.field.name,)
+        self.scores_field_name = '%s_scores' % (self.field.name,)
 
     def __get__(self, instance, type=None):
         if instance is None:
             raise AttributeError('Can only be accessed via an instance.')
+            # return self.field
         return RatingManager(instance, self.field)
 
 
 class RatingField(IntegerField):
     def __init__(self, *args, **kwargs):
-        if not 'choices' in kwargs:
+        if 'choices' not in kwargs:
             kwargs['choices'] = [1, 2]
         if not isinstance(kwargs['choices'], list):
-            raise TypeError("%s: invalid type for attribute 'choices'. "
-                            "Must be a list." % (self.__class__.__name__,))
+            raise TypeError('%s: invalid type for attribute `choices`. '
+                            'Must be a list.' % (self.__class__.__name__,))
         self.range = kwargs['choices']
         self.can_change_vote = kwargs.pop('can_change_vote', False)
         self.allow_anonymous = kwargs.pop('allow_anonymous', False)
@@ -286,31 +300,30 @@ class RatingField(IntegerField):
         kwargs['blank'] = True
         super(RatingField, self).__init__(*args, **kwargs)
 
-    def contribute_to_class(self, cls, name):
+    def contribute_to_class(self, cls, name, virtual_only=False):
         self.name = name
 
         # Computed rating
         self.score_field = FloatField(editable=False, db_index=True,
                                       default=0)
-        cls.add_to_class("%s_score" % (self.name,), self.score_field)
+        cls.add_to_class('%s_score' % (self.name,), self.score_field)
 
         self.score_day_field = FloatField(editable=False, db_index=True,
                                           default=0)
-        cls.add_to_class("%s_day" % (self.name,), self.score_day_field)
+        cls.add_to_class('%s_day' % (self.name,), self.score_day_field)
 
         self.score_week_field = FloatField(editable=False, db_index=True,
                                            default=0)
-        cls.add_to_class("%s_week" % (self.name,), self.score_week_field)
+        cls.add_to_class('%s_week' % (self.name,), self.score_week_field)
 
         self.score_month_field = FloatField(editable=False, db_index=True,
                                             default=0)
-        cls.add_to_class("%s_month" % (self.name,), self.score_month_field)
+        cls.add_to_class('%s_month' % (self.name,), self.score_month_field)
 
         # Each score qty [5,9]
         scores = [0] * len(self.range)
         self.scores_field = JSONField(editable=False, default=scores)
-        cls.add_to_class("%s_scores" % (self.name,), self.scores_field)
-
+        cls.add_to_class('%s_scores' % (self.name,), self.scores_field)
         self.key = md5_hexdigest(self.name)
 
         field = RatingCreator(self)
@@ -321,15 +334,16 @@ class RatingField(IntegerField):
 
         setattr(cls, name, field)
 
-    def get_db_prep_save(self, value):
+    def get_db_prep_save(self, value, connection):
         # XXX: what happens here?
         pass
 
-    def get_db_prep_lookup(self, lookup_type, value):
+    def get_db_prep_lookup(self, lookup_type, value, connection,
+                           prepared=False):
         raise NotImplementedError(self.get_db_prep_lookup)
 
     def formfield(self, **kwargs):
-        defaults = {'form_class': forms.RatingField}
+        defaults = {'form_class': RatingField}
         defaults.update(kwargs)
         return super(RatingField, self).formfield(**defaults)
 
@@ -337,3 +351,9 @@ class RatingField(IntegerField):
         if isinstance(self.range, list):
             return score in self.range
         return False
+
+
+class AnonymousRatingField(RatingField):
+    def __init__(self, *args, **kwargs):
+        kwargs['allow_anonymous'] = True
+        super(AnonymousRatingField, self).__init__(*args, **kwargs)
