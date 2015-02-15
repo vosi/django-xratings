1aaa43ac05973d9aeb84f1e6a896c9f26af9f995
Volodymyr Tartynskyi <vosi@users.noreply.github.com>
2015-02-16 00:22:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 kwargs = {
     'name': 'django-xratings',
-    'version': '0.0.1',
+    'version': ".".join(map(str, __import__('xratings').__version__)),
     'description': 'Django ratings system.',
-    'author': 'Tartnskyi Vladimir',
+    'author': 'Tartynskyi Volodymyr',
     'author_email': 'fon.vosi@gmail.com',
     'url': 'https://github.com/vosi/django-xratings',
     'keywords': 'django,ratings',
