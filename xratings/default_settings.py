24a20cb80880901f6ab8fe54ccd3e73880d5d4d1
Volodymyr Tartynskyi <vosi@users.noreply.github.com>
2015-02-15 22:58:39 +0200

    Update default_settings.py

--- xratings/default_settings.py
+++ xratings/default_settings.py
@@ -1,17 +1,18 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
 from datetime import timedelta
+
 from django.conf import settings
+
+# Used to limit the number of unique IPs that can vote on a single object+field.
+#   useful if you're getting rating spam by users registering multiple accounts
 from math import sqrt
 
-# Used to limit the number of unique IPs that can vote on a single
-# object+field.
-# useful if you're getting rating spam by users registering multiple accounts
-RATINGS_VOTES_PER_IP = getattr(settings, 'RATINGS_VOTES_PER_IP', 5555)
+RATINGS_VOTES_PER_IP = getattr(settings, 'RATINGS_VOTES_PER_IP', 3)
 RATINGS_VOTES_PER_IP_TIMEDELTA = getattr(settings,
                                          'RATINGS_VOTES_PER_IP_TIMEDELTA',
                                          timedelta(days=1))
-
-RATINGS_USER_MODEL = getattr(settings, 'RATINGS_USER_MODEL',
-                             settings.AUTH_USER_MODEL)
 
 
 def rating_bin_formula(scores, vrange):
@@ -25,21 +26,21 @@ def rating_bin_formula(scores, vrange):
     z = 1.25
     p = float(ups) / n
 
-    left = p + 1/(2*n)*z*z
-    right = z*sqrt(p*(1-p)/n + z*z/(4*n*n))
-    under = 1+1/n*z*z
-
+    left = p + 1 / (2 * n) * z * z
+    right = z * sqrt(p * (1 - p) / n + z * z / (4 * n * n))
+    under = 1 + 1 / n * z * z
     return (left - right) / under
 
+
 def rating_var_formula(scores, vrange):
-    if not(isinstance(scores, list) and isinstance(vrange, list)) or \
-       not (sum(scores) > 0 and len(vrange) > 0):
+    if (not(isinstance(scores, list) and isinstance(vrange, list)) or
+            not (sum(scores) > 0 and len(vrange) > 0)):
         return 0
     votes = float(sum(scores))
     vmin = float(0)
-    avg = sum([a*b for a,b in zip(vrange, scores)])/sum(scores)
+    avg = sum([a * b for a, b in zip(vrange, scores)]) / sum(scores)
     fix = 0.1
-    return (votes/(votes+vmin))*avg + (vmin/(votes + vmin))*fix
+    return (votes / (votes + vmin)) * avg + (vmin / (votes + vmin)) * fix
 
 RATINGS_DEFAULT_FORMULA = getattr(settings, 'RATINGS_DEFAULT_FORMULA',
                                   rating_bin_formula)
