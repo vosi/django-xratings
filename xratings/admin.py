f8e731a5de6adebeff55838fce5524a407489d18
Volodymyr Tartynskyi <vosi@users.noreply.github.com>
2015-02-16 00:16:18 +0200

    Update admin.py

--- xratings/admin.py
+++ xratings/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from ratings.models import Vote
+from xratings.models import Vote
 
 
 class VoteAdmin(admin.ModelAdmin):
