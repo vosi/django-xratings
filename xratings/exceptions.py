c152407d25767ad1f0c366f650d42985721586a6
Volodymyr Tartynskyi <vosi@users.noreply.github.com>
2015-02-15 22:58:51 +0200

    Update exceptions.py

--- xratings/exceptions.py
+++ xratings/exceptions.py
@@ -1,5 +1,22 @@
-class InvalidRating(ValueError): pass
-class AuthRequired(TypeError): pass
-class CannotChangeVote(Exception): pass
-class CannotDeleteVote(Exception): pass
-class IPLimitReached(Exception): pass
+# coding=utf-8
+from __future__ import unicode_literals
+
+
+class InvalidRating(ValueError):
+    pass
+
+
+class AuthRequired(TypeError):
+    pass
+
+
+class CannotChangeVote(Exception):
+    pass
+
+
+class CannotDeleteVote(Exception):
+    pass
+
+
+class IPLimitReached(Exception):
+    pass
