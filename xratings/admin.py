5f4d6ce9082b8e0e02decd1d9c464be6402666a0
Volodymyr Tartynskyi <vosi@users.noreply.github.com>
2015-02-15 22:58:08 +0200

    Update admin.py

--- xratings/admin.py
+++ xratings/admin.py
@@ -1,10 +1,17 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
 from django.contrib import admin
-from models import Vote
+
+from ratings.models import Vote
+
 
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ('content_object', 'user', 'ip_address', 'cookie', 'score', 'date_changed')
+    list_display = ('content_object', 'user', 'ip_address', 'cookie', 'score',
+                    'date_changed')
     list_filter = ('score', 'content_type', 'date_changed')
     search_fields = ('ip_address',)
     raw_id_fields = ('user',)
+
 
 admin.site.register(Vote, VoteAdmin)
